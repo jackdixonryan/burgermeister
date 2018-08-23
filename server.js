20d4c6138b39124ecafde79d60a2f441f35e652b
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-08-23 14:46:33 -0600

    Try again.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const hbars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
