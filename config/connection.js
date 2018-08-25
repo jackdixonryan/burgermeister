39408822805dbe166a8592661bd1e2cbc96a6c53
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-08-25 10:06:20 -0600

    Adding SQL database.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306 || process.env.PORT,
-    user: 'root',
-    password: 'Gl0rf1&d3l',
-    database: 'burgers_db'
-});
+let connection;
+
+if (process.env.JAWSDB_URL){
+    connection - mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306 || process.env.PORT,
+        user: 'root',
+        password: 'Gl0rf1&d3l',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(err => {
     if (err) throw err;
